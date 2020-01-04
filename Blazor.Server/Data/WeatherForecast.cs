9c30f93922fa0631dab0e005b7f7c79efad88581
Bonart <leo.bonart@gmail.com>
2020-01-04 10:12:00 -0800

    Rename WeatherForecast to WeatherForecastDto

--- Blazor.Server/Data/WeatherForecast.cs
+++ Blazor.Server/Data/WeatherForecast.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Blazor.Server.Data
 {
-	public class WeatherForecast
+	public class WeatherForecastDto
 	{
 		public DateTime Date { get; set; }
 
--- Blazor.Server/Data/WeatherForecastService.cs
+++ Blazor.Server/Data/WeatherForecastService.cs
@@ -12,13 +12,13 @@ namespace Blazor.Server.Data
 			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 		};
 
-		public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+		public Task<WeatherForecastDto[]> GetForecastAsync(DateTime startDate)
 		{
 			var rng = new Random();
 			return Task.FromResult(GetSampleForecast().ToArray());
 
-			IEnumerable<WeatherForecast> GetSampleForecast() 
-				=> Enumerable.Range(1, 5).Select(index => new WeatherForecast
+			IEnumerable<WeatherForecastDto> GetSampleForecast() 
+				=> Enumerable.Range(1, 5).Select(index => new WeatherForecastDto
 				{
 					Date = startDate.AddDays(index),
 					TemperatureC = rng.Next(-20, 55),
