1fdc06a0758bdba45613fc769e09ab1abf3fb71b
Bonart <leo.bonart@gmail.com>
2020-01-04 10:47:39 -0800

    Extract logic to Counter.razor.cs

---
+++ Blazor.Server/Pages/Counter.razor.cs
@@ -0,0 +1,20 @@
+﻿using Blazor.Server.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.Server.Pages
+{
+	public partial class Counter
+	{
+		private int currentCount = 0;
+
+		private void IncrementCount()
+		{
+			currentCount++;
+		}
+	}
+}
