47823c37233096e983ebe9e112f15901e4b22f39
Bonart <leo.bonart@gmail.com>
2020-01-04 10:57:12 -0800

    Add Db error page

--- Blazor.Server/Startup.cs
+++ Blazor.Server/Startup.cs
@@ -35,7 +35,10 @@ namespace Blazor.Server
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
             else
                 app.UseExceptionHandler("/Error");
 
