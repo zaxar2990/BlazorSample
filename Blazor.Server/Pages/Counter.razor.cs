bd74e815615cf4a0c0b058cfaf62cf79ce0a1e30
Bonart <leo.bonart@gmail.com>
2020-01-04 11:22:08 -0800

    WIP User registration hack

--- Blazor.Server/Pages/Counter.razor.cs
+++ Blazor.Server/Pages/Counter.razor.cs
@@ -10,11 +10,29 @@ namespace Blazor.Server.Pages
 {
 	public partial class Counter
 	{
+		[Inject] public ApplicationDbContext _db { get; set; }
+
 		private int currentCount = 0;
 
 		private void IncrementCount()
 		{
 			currentCount++;
+		}
+
+		
+
+		protected override async Task OnInitializedAsync()
+		{
+			//верификация почты
+			var v = await _db.Users.FirstOrDefaultAsync(u => u.Email == "2@2.ru");
+			v.EmailConfirmed = true;
+			_db.Update(v);
+			await _db.SaveChangesAsync();
+			var vv = await _db.Users.ToArrayAsync();
+
+
+
+			await base.OnInitializedAsync();
 		}
 	}
 }
